777b9815c68a6eb6a8c8a5f0255337d00b8fbeec
chaiheung <chaiheung@naver.com>
2024-05-27 12:19:35 +0900

    맨앞, 맨끝 페이지 버튼

--- src/page/board/BoardList.jsx
+++ src/page/board/BoardList.jsx
@@ -57,9 +57,14 @@ export function BoardList() {
       </Box>
       <Box>
         {pageInfo.prevPageNumber && (
-          <Button onClick={() => navigate(`/?page=${pageInfo.prevPageNumber}`)}>
-            이전
-          </Button>
+          <>
+            <Button onClick={() => navigate(`/?page=1`)}>처음</Button>
+            <Button
+              onClick={() => navigate(`/?page=${pageInfo.prevPageNumber}`)}
+            >
+              이전
+            </Button>
+          </>
         )}
         {pageNumbers.map((pageNumber) => (
           <Button
@@ -73,9 +78,18 @@ export function BoardList() {
           </Button>
         ))}
         {pageInfo.nextPageNumber && (
-          <Button onClick={() => navigate(`/?page=${pageInfo.nextPageNumber}`)}>
-            다음
-          </Button>
+          <>
+            <Button
+              onClick={() => navigate(`/?page=${pageInfo.nextPageNumber}`)}
+            >
+              다음
+            </Button>
+            <Button
+              onClick={() => navigate(`/?page=${pageInfo.lastPageNumber}`)}
+            >
+              맨끝
+            </Button>
+          </>
         )}
       </Box>
     </Box>
