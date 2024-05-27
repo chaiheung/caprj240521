72af4b813cb3aa871e8abe3c4bc16bf149c367bf
chaiheung <chaiheung@naver.com>
2024-05-27 09:18:07 +0900

    components -> component 디렉토리명 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,47 @@
 import React from "react";
-import {ChakraProvider} from "@chakra-ui/react";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import {Home} from "./page/Home.jsx";
-import {BoardWrite} from "./page/board/BoardWrite.jsx";
-import {BoardList} from "./page/board/BoardList.jsx";
-import {BoardView} from "./page/board/BoardView.jsx";
-import {BoardEdit} from "./page/board/BoardEdit.jsx";
-import {MemberSignup} from "./page/member/MemberSignup.jsx";
-import {MemberList} from "./page/member/MemberList.jsx";
-import {MemberInfo} from "./page/member/MemberInfo.jsx";
-import {MemberEdit} from "./page/member/MemberEdit.jsx";
-import {MemberLogin} from "./page/member/MemberLogin.jsx";
-import {LoginProvider} from "./component/LoginProvider.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./page/Home.jsx";
+import { BoardWrite } from "./page/board/BoardWrite.jsx";
+import { BoardList } from "./page/board/BoardList.jsx";
+import { BoardView } from "./page/board/BoardView.jsx";
+import { BoardEdit } from "./page/board/BoardEdit.jsx";
+import { MemberSignup } from "./page/member/MemberSignup.jsx";
+import { MemberList } from "./page/member/MemberList.jsx";
+import { MemberInfo } from "./page/member/MemberInfo.jsx";
+import { MemberEdit } from "./page/member/MemberEdit.jsx";
+import { MemberLogin } from "./page/member/MemberLogin.jsx";
+import { LoginProvider } from "./component/LoginProvider.jsx";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home/>,
-        children: [
-            {
-                index: true,
-                element: <BoardList/>,
-            },
-            {path: "write", element: <BoardWrite/>},
-            {path: "board/:id", element: <BoardView/>},
-            {path: "edit/:id", element: <BoardEdit/>},
-            {path: "signup", element: <MemberSignup/>},
-            {path: "member/list", element: <MemberList/>},
-            {path: "member/:id", element: <MemberInfo/>},
-            {path: "member/edit/:id", element: <MemberEdit/>},
-            {path: "login", element: <MemberLogin/>},
-        ],
-    },
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        index: true,
+        element: <BoardList />,
+      },
+      { path: "write", element: <BoardWrite /> },
+      { path: "board/:id", element: <BoardView /> },
+      { path: "edit/:id", element: <BoardEdit /> },
+      { path: "signup", element: <MemberSignup /> },
+      { path: "member/list", element: <MemberList /> },
+      { path: "member/:id", element: <MemberInfo /> },
+      { path: "member/edit/:id", element: <MemberEdit /> },
+      { path: "login", element: <MemberLogin /> },
+    ],
+  },
 ]);
 
 function App(props) {
-    return (
-        <LoginProvider>
-            <ChakraProvider>
-                <RouterProvider router={router}/>
-            </ChakraProvider>
-        </LoginProvider>
-    );
+  return (
+    <LoginProvider>
+      <ChakraProvider>
+        <RouterProvider router={router} />
+      </ChakraProvider>
+    </LoginProvider>
+  );
 }
 
 export default App;
