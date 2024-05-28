4bcc78320a45484020e58e3691ac6fb773ae06b8
chaiheung <chaiheung@naver.com>
2024-05-28 09:26:37 +0900

    파일 선택 시 선택된 파일 출력

--- src/page/board/BoardWrite.jsx
+++ src/page/board/BoardWrite.jsx
@@ -1,95 +1,126 @@
-import {Box, Button, FormControl, FormLabel, Input, Textarea, useToast,} from "@chakra-ui/react";
-import React, {useContext, useState} from "react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+  useToast,
+} from "@chakra-ui/react";
+import React, { useContext, useState } from "react";
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
-import {LoginContext} from "../../component/LoginProvider.jsx";
+import { useNavigate } from "react-router-dom";
+import { LoginContext } from "../../component/LoginProvider.jsx";
 
 export function BoardWrite() {
-    const [title, setTitle] = useState("");
-    const [content, setContent] = useState("");
-    const [loading, setLoading] = useState(false);
-    const account = useContext(LoginContext);
-    const toast = useToast();
-    const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const account = useContext(LoginContext);
+  const toast = useToast();
+  const navigate = useNavigate();
 
-    function handleSaveClick() {
-        setLoading(true);
-        axios
-            .post(
-                "/api/board/add",
-                {
-                    title,
-                    content,
-                },
-                {
-                    headers: {
-                        Authorization: `Bearer ${localStorage.getItem("token")}`,
-                    },
-                },
-            )
-            .then(() => {
-                toast({
-                    description: "새 글이 등록되었습니다.",
-                    status: "success",
-                    position: "top",
-                });
-                navigate("/");
-            })
-            .catch((e) => {
-                const code = e.response.status;
+  function handleSaveClick() {
+    setLoading(true);
+    axios
+      .post(
+        "/api/board/add",
+        {
+          title,
+          content,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        },
+      )
+      .then(() => {
+        toast({
+          description: "새 글이 등록되었습니다.",
+          status: "success",
+          position: "top",
+        });
+        navigate("/");
+      })
+      .catch((e) => {
+        const code = e.response.status;
 
-                if (code === 400) {
-                    toast({
-                        status: "error",
-                        description: "등록되지 않았습니다. 입력한 내용을 확인하세요.",
-                        position: "top",
-                    });
-                }
-            })
-            .finally(() => setLoading(false));
-    }
+        if (code === 400) {
+          toast({
+            status: "error",
+            description: "등록되지 않았습니다. 입력한 내용을 확인하세요.",
+            position: "top",
+          });
+        }
+      })
+      .finally(() => setLoading(false));
+  }
 
-    let disableSaveButton = false;
-    if (title.trim().length === 0) {
-        disableSaveButton = true;
-    }
-    if (content.trim().length === 0) {
-        disableSaveButton = true;
-    }
+  let disableSaveButton = false;
+  if (title.trim().length === 0) {
+    disableSaveButton = true;
+  }
+  if (content.trim().length === 0) {
+    disableSaveButton = true;
+  }
 
-    return (
+  // file 목록 작성
+  const fileNameList = [];
+  for (let i = 0; i < files.length; i++) {
+    fileNameList.push(<li>{files[i].name}</li>);
+  }
+
+  return (
+    <Box>
+      <Box>글 작성 화면</Box>
+      <Box>
         <Box>
-            <Box>글 작성 화면</Box>
-            <Box>
-                <Box>
-                    <FormControl>
-                        <FormLabel>제목</FormLabel>
-                        <Input onChange={(e) => setTitle(e.target.value)}/>
-                    </FormControl>
-                </Box>
-                <Box>
-                    <FormControl>
-                        <FormLabel>본문</FormLabel>
-                        <Textarea onChange={(e) => setContent(e.target.value)}/>
-                    </FormControl>
-                </Box>
-                <Box>
-                    <FormControl>
-                        <FormLabel>작성자</FormLabel>
-                        <Input readOnly value={account.nickName}/>
-                    </FormControl>
-                </Box>
-                <Box>
-                    <Button
-                        isLoading={loading}
-                        isDisabled={disableSaveButton}
-                        colorScheme={"blue"}
-                        onClick={handleSaveClick}
-                    >
-                        저장
-                    </Button>
-                </Box>
-            </Box>
+          <FormControl>
+            <FormLabel>제목</FormLabel>
+            <Input onChange={(e) => setTitle(e.target.value)} />
+          </FormControl>
         </Box>
-    );
+        <Box>
+          <FormControl>
+            <FormLabel>본문</FormLabel>
+            <Textarea onChange={(e) => setContent(e.target.value)} />
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>파일</FormLabel>
+            <Input
+              multiple
+              type="file"
+              accept="image/*"
+              onChange={(e) => {
+                setFiles(e.target.files);
+              }}
+            />
+          </FormControl>
+        </Box>
+        <Box>
+          <ul>{fileNameList}</ul>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>작성자</FormLabel>
+            <Input readOnly value={account.nickName} />
+          </FormControl>
+        </Box>
+        <Box>
+          <Button
+            isLoading={loading}
+            isDisabled={disableSaveButton}
+            colorScheme={"blue"}
+            onClick={handleSaveClick}
+          >
+            저장
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
