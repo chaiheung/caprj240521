db04efc20a01e302bdd86aa72c1944d581fba037
chaiheung <chaiheung@naver.com>
2024-05-30 16:34:23 +0900

    댓글 삭제 시 모달로 확인, 삭제 후 토스트

--- src/component/comment/CommentItem.jsx
+++ src/component/comment/CommentItem.jsx
@@ -1,10 +1,26 @@
-import { Box, Button, Flex, Spacer } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Spacer,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons/faXmark";
 import axios from "axios";
 
 export function CommentItem({ comment, isProcessing, setIsProcessing }) {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const toast = useToast();
+
   function handleRemoveClick() {
     setIsProcessing(true);
     axios
@@ -14,7 +30,13 @@ export function CommentItem({ comment, isProcessing, setIsProcessing }) {
       .then((res) => {})
       .catch((err) => {})
       .finally(() => {
+        onClose();
         setIsProcessing(false);
+        toast({
+          description: "댓글이 삭제되었습니다.",
+          status: "info",
+          position: "top",
+        });
       });
   }
 
@@ -29,15 +51,28 @@ export function CommentItem({ comment, isProcessing, setIsProcessing }) {
         <Box>{comment.comment}</Box>
         <Spacer />
         <Box>
-          <Button
-            isLoading={isProcessing}
-            colorScheme={"red"}
-            onClick={handleRemoveClick}
-          >
+          <Button isLoading={isProcessing} colorScheme={"red"} onClick={onOpen}>
             <FontAwesomeIcon icon={faXmark} />
           </Button>
         </Box>
       </Flex>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>삭제 확인</ModalHeader>
+          <ModalBody>댓글을 삭제 하시겠습니까?</ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>취소</Button>
+            <Button
+              isLoading={isProcessing}
+              colorScheme={"red"}
+              onClick={handleRemoveClick}
+            >
+              삭제
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
